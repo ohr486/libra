4b3be18dd10059601e3826f28e4fbd85136d0daf
ohr486 <ohr486@gmail.com>
2018-03-06 21:05:14 +0900

    update mix

--- mix.exs
+++ mix.exs
@@ -21,8 +21,11 @@ defmodule Libra.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      # for test
+      {:credo, "~> 0.8.1", only: [:dev, :test]},
+      {:dialyxir, "~> 0.5.1", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.7.0", only: :test},
+      {:mix_test_watch, "~> 0.5.0", only: :dev, runtime: false}
     ]
   end
 end
